87e3fa9b7a2292add6c41436156a3f12046c955e
INGJUNIOR <INGJUNIOR@users.noreply.github.com>
2026-03-24 02:32:32 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-function test() {
-  console.log("Hello CodeQL");
+function sum(a, b) {
+  return a + b;
 }
+
+function test() {
+  let result = sum(5, 3);
+  console.log("Resultado:", result);
+}
+
 test();
